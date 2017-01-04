71bd64924ccd98ea9f59d4b02fd7228ab0798c05
linyandi <wwwlinyandi@126.com>
2017-01-04 14:12:55 +0800

    return string when input is string

--- index.js
+++ index.js
@@ -6,18 +6,21 @@
  * date  : 2016-12-15
  *
  */
-var fs = require ("fs");
-var path = require ("path");
-var loaderUtils = require ("loader-utils");
+let fs = require ("fs");
+let path = require ("path");
+let loaderUtils = require ("loader-utils");
+let hasToString = false;
 
 module.exports = function (content) {
 
   this.cacheable && this.cacheable ();
-  
+
   if (/module\.exports\s?=/.test(content)) {
     content = content.replace(/module\.exports\s?=\s?/, '').replace(/\\n/g, '').replace(/\\/g, '').replace(/";$/, '').replace(/^"/, '');
+  } else {
+    hasToString = true;
   }
-  
+
   let reg = /#\{require\([^)]*\)\}/g;
   let regLeft = /#\{require\(/g;
   let regLast = /\)\}$/g
@@ -25,27 +28,30 @@ module.exports = function (content) {
   let ObjectReg = new RegExp(/^\{[^\}]\}$/,"g");
   let temp = "";
   let regModule = /module\.exports\s*=\s*/g;
-  
+
   let query = loaderUtils.parseQuery (this.query) || {root: "./src"};
-  
+
   let source = content.replace (reg, function (match) {
     let filePath = "";
     let fileData = "";
     let res = match.replace (regLeft, "").replace (regLast, "");
     let option = res.match (regOpt) ? JSON.parse (res.match (regOpt)[0]) : null;
     let filePathStr = res.replace (regOpt, "").replace (",", "").replace (/"/g, "").replace (/\s/g, "");
-    
+
     filePath = path.resolve (query.root, filePathStr);
-    
+
     fileData = fs.readFileSync(filePath, {encoding: "utf-8"});
-    
+
     for (key in option) {
       fileData = fileData.replace(new RegExp("#{" + key + "}","g"), option[key]);
     }
-    
+
     return fileData;
-    
+
   });
-  
-  return source;
+  if (!hasToString){
+    return 'module.exports = ' + JSON.stringify(source) + ';';
+  } else {
+    return source;
+  }
 };
